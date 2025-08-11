bc72dccc72807e951bda2b22b768878a6dfc4d46
Neelendra Shukla <neelendrashukla07@gmail.com>
2025-08-11 14:05:59 +0530

    animated navbar.jsx with navbar_animation.css

--- team5_webdev/src/components/Navbaar.jsx
+++ team5_webdev/src/components/Navbaar.jsx
@@ -5,8 +5,8 @@ const Navbaar = () => {
     <div>
         <center>
       <div className="flex justify-between items-center w-250 mt-8">
-        <div className="text-4xl font-bold">INDIA</div>
-        <div>
+        <div className="text-4xl font-bold slide-up-animation">INDIA</div>
+        <div className="slide-up-animation">
             <ul className="flex gap-20 text-xl">
                 <li><a className="text-orange-500" href="#home">Home</a></li>
                 <li><a className="text-orange-500" href="">About</a></li>
