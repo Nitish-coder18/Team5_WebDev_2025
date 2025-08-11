061c552a72bae4f4c997e5803ab5e51cbb584759
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-08-11 14:01:24 +0530

    Add Navbaar component and update Header layout

--- team5_webdev/src/components/Header.jsx
+++ team5_webdev/src/components/Header.jsx
@@ -1,20 +1,23 @@
 import image from "..//assets/indianfleag2.png"
 import "./header_animation.css";
+import Navbaar from "./Navbaar";
+
 
 const Header = () => {
   return (
-
-    <div className="flex flex-row items-center justify-center gap-50 mt-35">
+    <>
+    <Navbaar />
+    <div id="home" className="flex flex-row items-center justify-center gap-60 mt-35">
       <div className="">
             <h1 className="text-5xl font-bold mb-4 text-orange-500 slide-up-animation"> Digital Freedom Gallery</h1>
             <p className="text-3xl mb-14 items-start mt-3 slide-up-animation">Code a Vision. Design the Nation</p>
             <p className="text-2xl font-extralight text-green-500 slide-up-animation">A web tribute to India's <br /> past, present, and digital future</p>
       </div>
       <div className="updown-animation">
-          <img className="h-120 slide-up-animation" src={image} alt="" />
+          <img className="h-90 slide-up-animation" src={image} alt="" />
       </div>
     </div>
-
+    </>
   )
 }
 
