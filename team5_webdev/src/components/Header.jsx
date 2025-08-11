b3fe9725ca2ff4aa5ef4383b0a885a908c7670ee
Neelendra Shukla <neelendrashukla07@gmail.com>
2025-08-11 13:43:01 +0530

    added animations to header.jsx file

--- team5_webdev/src/components/Header.jsx
+++ team5_webdev/src/components/Header.jsx
@@ -1,16 +1,17 @@
 import image from "..//assets/indianfleag2.png"
+import "./header_animation.css";
 
 const Header = () => {
   return (
 
     <div className="flex flex-row items-center justify-center gap-50 mt-35">
       <div className="">
-            <h1 className="text-5xl font-bold mb-4 text-orange-500 "> Digital Freedom Gallery</h1>
-            <p className="text-3xl mb-14 items-start mt-3">Code a Vision. Design the Nation</p>
-            <p className="text-2xl font-extralight text-green-500 ">A web tribute to India's <br /> past, present, and digital future</p>
+            <h1 className="text-5xl font-bold mb-4 text-orange-500 slide-up-animation"> Digital Freedom Gallery</h1>
+            <p className="text-3xl mb-14 items-start mt-3 slide-up-animation">Code a Vision. Design the Nation</p>
+            <p className="text-2xl font-extralight text-green-500 slide-up-animation">A web tribute to India's <br /> past, present, and digital future</p>
       </div>
-      <div>
-          <img className="h-120" src={image} alt="" />
+      <div className="updown-animation">
+          <img className="h-120 slide-up-animation" src={image} alt="" />
       </div>
     </div>
 
